1982b84e84b2e882b7d1b599ba886194c23ca7e9
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2024-11-20 13:42:55 +0000

    camera room snapping implemented

---
+++ Game/Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject camera;
+    private Vector2 roomDimensions = new Vector2(32, 18);
+
+    private void Awake()
+    {
+        Debug.Log((roomDimensions.x) * (math.floor(-2 / 32)));
+        Debug.Log(Mathf.Floor(-2 / 32));
+        camera = Camera.main.gameObject;
+    }
+
+    void Update()
+    {
+        Vector2 playerPos = this.transform.position;
+
+        int roomX = Mathf.FloorToInt((playerPos.x + roomDimensions.x / 2) / roomDimensions.x);
+        int roomY = Mathf.FloorToInt((playerPos.y + roomDimensions.y / 2) / roomDimensions.y);
+
+        float cameraX = roomX * roomDimensions.x;
+        float cameraY = roomY * roomDimensions.y;
+
+        if (camera.transform.position.x != cameraX || camera.transform.position.y != cameraY)
+        {
+            camera.transform.position = new Vector3(cameraX, cameraY, -10f);
+        }
+    }
+
+}
