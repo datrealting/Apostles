cc2f5b3f27547dc371afde86d48ac359080a9e16
datrealting <150270914+datrealting@users.noreply.github.com>
2024-11-27 23:03:06 +0000

    Added Music

--- Game/Assets/Death/GameManager.cs
+++ Game/Assets/Death/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,6 +8,16 @@ public class GameManager : MonoBehaviour
 
     public int playerSouls = 0; // Persistent souls
     public WeaponStats weaponStats;
+    public DangerState dangerState;
+
+    // Music variables
+    public AudioSource bass1;
+    public AudioSource bass2;
+    public AudioSource dr1;
+    public AudioSource dr2;
+    public AudioSource dr3;
+    public AudioSource g1;
+    public AudioSource g2;
 
     private void Awake()
     {
@@ -19,5 +31,37 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject); // Ensures only one instance exists
         }
+        HandleMusic(dangerState);
+    }
+    private void HandleMusic(DangerState dangerState)
+    {
+        switch (dangerState) 
+        {
+            case DangerState.Low:
+                bass1.volume = 1;
+                dr1.volume = 1;
+                break;
+            case DangerState.Medium:
+                bass1.volume = 1;
+                dr1.volume = 1;
+                bass2.volume = 1;
+                dr2.volume = 1;
+                break;
+            case DangerState.High:
+                bass1.volume = 1;
+                dr1.volume = 1;
+                bass2.volume = 1;
+                dr2.volume = 1;
+                dr3.volume = 1;
+                g1.volume = 1;
+                g2.volume = 1;
+                break;
+        }
+    }
+    public enum DangerState
+    {
+        Low,
+        Medium,
+        High
     }
 }
