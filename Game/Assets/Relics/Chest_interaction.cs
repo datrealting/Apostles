1770000544835796709b8bafc00d0942e8083124
Sickkiwi <w21001035@northumbria.ac.uk>
2024-12-15 22:34:17 +0000

    fixed chest spawn

--- Game/Assets/Relics/Chest_interaction.cs
+++ Game/Assets/Relics/Chest_interaction.cs
@@ -1,16 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Chest_interaction : MonoBehaviour
 {
     public List<GameObject> prefabList = new List<GameObject>();
-
+    public Vector3 offSet = new Vector3(0, -2, 0);
+    
     void Update(){
         if (Input.GetKey("e")){
+            
             int prefabIndex = UnityEngine.Random.Range(0,prefabList.Count-1);
             print(prefabIndex);
             print(prefabList);
-            Instantiate(prefabList[prefabIndex]);
+            Instantiate(prefabList[prefabIndex],transform.position + offSet, transform.rotation);
             gameObject.GetComponent<ChestOpening>().isOpen = true;
             gameObject.GetComponent<Chest_interaction>().enabled = false;
             
