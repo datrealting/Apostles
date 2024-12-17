b7a82ea4b8146aeb371dfd4e979af876397cbd2c
mvprapid <w21047229@northumbria.ac.uk>
2024-12-17 02:04:25 +0000

    Fixed melee weapons
    Melee weapons are now usable but they need animations

--- Game/Assets/Scripts/CombatSystem/Weapon System/MeleeWeapon.cs
+++ Game/Assets/Scripts/CombatSystem/Weapon System/MeleeWeapon.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class MeleeWeapon : Weapon
 {
     public float range = 0f;
@@ -20,17 +20,19 @@ public class MeleeWeapon : Weapon
     {
         float halfQuarterCircleAngle = quarterCircleAngle / 2f;
         float angleStep = quarterCircleAngle / (rayCount - 1);
+        int enemyLayer = LayerMask.GetMask("Enemy");
+        HashSet<Collider2D> hitEnemies = new HashSet<Collider2D>();
 
         for (int i = 0; i < rayCount; i++)
         {
             float angle = -halfQuarterCircleAngle + (angleStep * i);
             Vector2 direction = Quaternion.Euler(0, 0, angle) * attackTransform.up;
-            RaycastHit2D hit = Physics2D.Raycast(attackTransform.position, direction, range);
+            RaycastHit2D hit = Physics2D.Raycast(attackTransform.position, direction, range, enemyLayer);
 
             // Draw the ray for debugging purposes
             Debug.DrawRay(attackTransform.position, direction * range, Color.red, 0.2f);
 
-            if (hit.collider != null)
+            if (hit.collider != null && !hitEnemies.Contains(hit.collider))
             {
                 Debug.Log("[Attack] Hit: " + hit.collider.name + " at distance: " + hit.distance);
 
@@ -38,6 +40,7 @@ public class MeleeWeapon : Weapon
                 {
                     hit.collider.GetComponent<NPCStats>()?.TakeDamage(weaponStats.dmg);  // Use the damage passed from the weapon
                     GameObject.Find("Player").GetComponent<PlayerControl>().onStrike?.Invoke(hit.collider.gameObject);
+                    hitEnemies.Add(hit.collider);
                 }
             }
             else
