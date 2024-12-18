3b5ba8706c6ddd1e4051bcd3d68939e2364aa1d3
mvprapid <w21047229@northumbria.ac.uk>
2024-12-18 13:58:55 +0000

    Added Effect to Melee attack

--- Game/Assets/Scripts/CombatSystem/Weapon System/MeleeWeapon.cs
+++ Game/Assets/Scripts/CombatSystem/Weapon System/MeleeWeapon.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+
 public class MeleeWeapon : Weapon
 {
     public float range = 0f;
-    public float quarterCircleAngle = 90f; // Angle of the quarter circle in degrees
+    public float weaponSwingArcAngle = 90f; // the  
+    // Angle of the quarter circle in degrees
     public int rayCount = 10; // Number of rays to cast within the quarter circle
     public Transform attackTransform;
+    private LineRenderer lineRenderer;
 
- 
+    protected override void Start()
+    {
+        base.Start();
+        // Add and configure the LineRenderer component
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = rayCount + 1;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.yellow;
+        lineRenderer.endColor = Color.yellow;
+        lineRenderer.enabled = false; // Hide the line initially
+        lineRenderer.sortingOrder = -1; // Set the order in layer to -1
+    }
 
     // Update is called once per frame
     protected override void Update()
@@ -18,10 +36,13 @@ public class MeleeWeapon : Weapon
 
     public override void Attack()
     {
-        float halfQuarterCircleAngle = quarterCircleAngle / 2f;
-        float angleStep = quarterCircleAngle / (rayCount - 1);
+        float halfQuarterCircleAngle = weaponSwingArcAngle / 2f;
+        float angleStep = weaponSwingArcAngle / (rayCount - 1);
         int enemyLayer = LayerMask.GetMask("Enemy");
         HashSet<Collider2D> hitEnemies = new HashSet<Collider2D>();
+
+        // Show the line renderer
+        StartCoroutine(ShowLineRenderer());
 
         for (int i = 0; i < rayCount; i++)
         {
@@ -47,6 +68,29 @@ public class MeleeWeapon : Weapon
             {
                 Debug.Log("[Attack] No hit detected for ray at angle: " + angle);
             }
+        }
+    }
+
+    private IEnumerator ShowLineRenderer()
+    {
+        UpdateLineRenderer();
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        lineRenderer.enabled = false;
+    }
+
+    private void UpdateLineRenderer()
+    {
+        float halfQuarterCircleAngle = weaponSwingArcAngle / 2f;
+        float angleStep = weaponSwingArcAngle / (rayCount - 1);
+
+        lineRenderer.SetPosition(0, attackTransform.position);
+
+        for (int i = 0; i < rayCount; i++)
+        {
+            float angle = -halfQuarterCircleAngle + (angleStep * i);
+            Vector3 direction = Quaternion.Euler(0, 0, angle) * attackTransform.up * range;
+            lineRenderer.SetPosition(i + 1, attackTransform.position + direction);
         }
     }
 }
